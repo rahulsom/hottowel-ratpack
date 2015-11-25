385d336247c8a139bc39099aa964f08afb276ef8
Rahul Somasunderam <rsom@certifydatasystems.com>
2015-11-25 11:44:35 -0800

    Reduce js loading

---
+++ src/assets/js/lib.js
@@ -0,0 +1,8 @@
+//= require /bower_components/jquery/jquery
+//= require /bower_components/angular/angular
+//= require /bower_components/angular-sanitize/angular-sanitize
+//= require /bower_components/extras.angular.plus/ngplus-overlay
+//= require /bower_components/moment/moment
+//= require /bower_components/angular-ui-router/angular-ui-router
+//= require /bower_components/toastr/toastr
+//= require /bower_components/angular-animate/angular-animate
