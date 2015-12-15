3144d30732c38db0a83c60abdc3c0ccdf379e594
Rahul Somasunderam <rsom@certifydatasystems.com>
2015-12-14 16:59:27 -0800

    Make /audittrail work in production too
    Conflicts:
    	src/ratpack/ratpack.groovy

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -1,9 +1,10 @@
 import asset.pipeline.ratpack.AssetPipelineHandler
 import asset.pipeline.ratpack.AssetPipelineModule
-import static ratpack.jackson.Jackson.json
 import ratpack.hottowel.MockData
+import ratpack.path.internal.RootPathBinding
 
 import static ratpack.groovy.Groovy.ratpack
+import static ratpack.jackson.Jackson.json
 
 ratpack {
   bindings {
@@ -17,5 +18,8 @@ ratpack {
       render(json(MockData.people))
     }
     all (new AssetPipelineHandler())
+    all { ctx ->
+      ctx.insert(single(new RootPathBinding("index.html")),new AssetPipelineHandler())
+    }
   }
 }
