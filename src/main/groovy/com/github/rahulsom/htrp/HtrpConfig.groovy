7a2cf6eac60e8b483682d690cc4cc1f5567b2694
Rahul Somasunderam <rahul.som@gmail.com>
2016-03-19 21:46:41 -0700

    Add support for rp-config

---
+++ src/main/groovy/com/github/rahulsom/htrp/HtrpConfig.groovy
@@ -0,0 +1,8 @@
+package com.github.rahulsom.htrp
+
+/**
+ * Created by rahul on 3/19/16.
+ */
+class HtrpConfig {
+    String someValue
+}
--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -1,25 +1,29 @@
 import asset.pipeline.ratpack.AssetPipelineHandler
 import asset.pipeline.ratpack.AssetPipelineModule
+import com.github.rahulsom.htrp.HtrpConfig
 import com.github.rahulsom.htrp.MockData
+import com.github.rahulsom.rpconfig.ConfigHandler
+import com.github.rahulsom.rpconfig.ConfigModule
 import ratpack.path.internal.RootPathBinding
 
 import static ratpack.groovy.Groovy.ratpack
 import static ratpack.jackson.Jackson.json
 
 ratpack {
-  bindings {
-    module(new AssetPipelineModule()) {
-      it.url("/")
-      it.sourcePath("../../../src/assets")
+    bindings {
+        module(AssetPipelineModule) {
+            it.url "/" sourcePath "../../../src/assets"
+        }
+        module new ConfigModule(HtrpConfig, "HTRP_", 'htrp.')
     }
-  }
-  handlers {
-    get("api/people") {
-      render(json(MockData.people))
+    handlers {
+        get 'api/config', ConfigHandler
+        get("api/people") {
+            render json(MockData.people)
+        }
+        all AssetPipelineHandler
+        all { ctx ->
+            ctx.insert single(new RootPathBinding("index.html")), new AssetPipelineHandler()
+        }
     }
-    all (new AssetPipelineHandler())
-    all { ctx ->
-      ctx.insert(single(new RootPathBinding("index.html")),new AssetPipelineHandler())
-    }
-  }
 }
